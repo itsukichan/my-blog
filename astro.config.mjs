cdcc98ed3203fec4160aa68feefa38d42aaec32e
itsukichan <kou05ituki09design@gmail.com>
2025-02-22 16:51:44 +0900

    fix: vue, sitemap削除,検索から除外

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vue from "@astrojs/vue";
-import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://techpanda.netlify.app/",
   integrations: [
-    vue(),
     tailwind({
       config: { applyBaseStyles: false },
     }),
-    sitemap(),
     compress(),
   ],
   image: {
